201ccaad7ed27825592f5fdcf3802c30b383a15b
chenyq <532788057@qq.com>
2019-08-10 18:35:56 +0800

    update table

--- PC端表格组件/js/table.js
+++ PC端表格组件/js/table.js
@@ -1,25 +1,28 @@
 /*固定表格滑动脚本开始*/
-function tableSwiperFunc(init) {
+function tableSwiperFunc() {
 	/*表格主滑动区域*/
-	var wrap_body = init.wrap_body;
-	var scroll_body = init.scroll_body;
+	var wrap_body = $(".scroll-table-body")[0];
+	var scroll_body = $(".scroll-table-body table")[0];
 	var wrap_body_width = 0;
 	var wrap_body_height = 0;
 	var scroll_body_width = 0;
 	var scroll_body_height = 0;
 	/*表格头部区域*/
-	var wrap_head = init.wrap_head;
-	var scroll_head = init.scroll_head;
+	var wrap_head = $(".scroll-table-head")[0];
+	var scroll_head = $(".scroll-table-head table")[0];
 	/*表格左边固定区域*/
-	var left_wrap_body = init.left_wrap_body;
-	var left_scroll_body = init.left_scroll_body;
+	var left_wrap_body = $(".scroll-table-fixed-left-body")[0];
+	var left_scroll_body = $(".scroll-table-fixed-left-body table")[0];
 
 	transform(scroll_body, "translateX", 0);
 	transform(scroll_body, "translateY", 0);
 	transform(scroll_head, "translateX", 0);
 	transform(left_scroll_body, "translateY", 0);
-
+	if ($(scroll_body).find('tbody').children().length == 0) {
+		$('.scroll-table-box').css({display: 'none'});
+	}
 	/* 竖向滚动条 */
+	var vertical_bar = $('.v-scroll-bar')[0];
 	var verticalBarHeight = 0; // 设置竖向滚动条的初始高度
 	var verticalBoxHeight = 0; // 设置竖向滚动条外层父级的高度
 	var verticalBarRange = []; // 竖向滚动条滚动范围
@@ -31,15 +34,15 @@ function tableSwiperFunc(init) {
 		verticalBoxHeight = verticalBoxAllHeight - 44; // 可滑动的区域高度
 		if (scroll_body_height <= wrap_body_height) {
 			verticalBarHeight = 0;
-			$(init.vertical_bar).parent().css({ height: '0', visibility: 'hidden' });
+			$(vertical_bar).parent().css({ height: '0', visibility: 'hidden' });
 		} else {
 			verticalBarHeight = (wrap_body_height / scroll_body_height) * verticalBoxHeight;
-			$(init.vertical_bar).parent().css({ height: verticalBoxAllHeight, visibility: 'visible', top: $(wrap_head).height() });
+			$(vertical_bar).parent().css({ height: verticalBoxAllHeight, visibility: 'visible', top: $(wrap_head).height() });
 			if (verticalBarHeight < 50) {
 				verticalBarHeight = 50;
 			}
 		}
-		$(init.vertical_bar).height(verticalBarHeight);
+		$(vertical_bar).height(verticalBarHeight);
 		verticalBarRange = [22, verticalBoxHeight - verticalBarHeight + 22];
 		verticalAreaRange = [0, scroll_body_height - wrap_body_height];
 	}
@@ -52,7 +55,7 @@ function tableSwiperFunc(init) {
 	function down1(e) {
 		setVerticalBar();
 		startY = e.clientY;
-		top = $(init.vertical_bar).position().top;
+		top = $(vertical_bar).position().top;
 		function move(e) {
 			var newTop = 0;
 			endY = e.clientY;
@@ -62,7 +65,7 @@ function tableSwiperFunc(init) {
 			} else if (newTop < verticalBarRange[0]) {
 				newTop = verticalBarRange[0];
 			}
-			$(init.vertical_bar).css({
+			$(vertical_bar).css({
 				top: newTop
 			});
 			elemTargetY = verticalAreaRange[1] * ((newTop - verticalBarRange[0]) / (verticalBarRange[1] - verticalBarRange[0]));
@@ -74,23 +77,23 @@ function tableSwiperFunc(init) {
 			$(document).off('mousemove');
 			$(document).off('mouseup');
 			//释放捕获
-			init.vertical_bar.releaseCapture && init.vertical_bar.releaseCapture();
+			vertical_bar.releaseCapture && vertical_bar.releaseCapture();
 		}
 		$(document).on('mousemove', move);
 		$(document).on('mouseup', up);
 		//设置捕获
-		init.vertical_bar.setCapture && init.vertical_bar.setCapture();
+		vertical_bar.setCapture && vertical_bar.setCapture();
 		//阻止浏览器默认事件
 		e.preventDefault && e.preventDefault();
 		return false;
 	}
-	$(init.vertical_bar).on('mousedown', down1);
+	$(vertical_bar).on('mousedown', down1);
 	//竖向滚动条滚轮事件
 	$('.scroll-table-box').on("mousewheel DOMMouseScroll", function (e) {
 		var delta = (e.originalEvent.wheelDelta && (e.originalEvent.wheelDelta > 0 ? 1 : -1)) ||
 			(e.originalEvent.detail && (e.originalEvent.detail > 0 ? -1 : 1));
 		var dir = '';
-		top = $(init.vertical_bar).position().top;
+		top = $(vertical_bar).position().top;
 		if (scroll_body_height <= wrap_body_height) {
 			return;
 		}
@@ -107,7 +110,7 @@ function tableSwiperFunc(init) {
 					top = verticalBarRange[1];
 				}
 			}
-			$(init.vertical_bar).css({
+			$(vertical_bar).css({
 				top: top
 			});
 			elemTargetY = verticalAreaRange[1] * ((top - verticalBarRange[0]) / (verticalBarRange[1] - verticalBarRange[0]));
@@ -119,7 +122,7 @@ function tableSwiperFunc(init) {
 	//竖向滚动条点击滚动区域自动定位功能
 	$('.v-scroll-bar-box').on('click', function (e) {
 		var clickPos = e.clientY + $(window).scrollTop() - $(this).offset().top;
-		var barTop = parseInt($(init.vertical_bar).css('top'));
+		var barTop = parseInt($(vertical_bar).css('top'));
 		var newTop = 0;
 		//确定新的滚动条top值
 		if (clickPos <= barTop) {
@@ -134,7 +137,7 @@ function tableSwiperFunc(init) {
 			}
 		}
 		if (e.target == $(this)[0]) {
-			$(init.vertical_bar).css({
+			$(vertical_bar).css({
 				transition: 'top .3s',
 				top: newTop
 			});
@@ -148,7 +151,7 @@ function tableSwiperFunc(init) {
 			transform(scroll_body, "translateY", -elemTargetY);
 			transform(left_scroll_body, "translateY", -elemTargetY);
 			setTimeout(function () {
-				$(init.vertical_bar).css({
+				$(vertical_bar).css({
 					transition: 'none',
 				});
 				$(left_scroll_body).css({
@@ -163,6 +166,7 @@ function tableSwiperFunc(init) {
 	});
 
 	/* 横向滚动条 */
+	var horizontal_bar = $('.scroll-bar')[0];
 	var horizontalBarWidth = 0; // 设置横向滚动条的初始宽度
 	var horizontalBoxWidth = 0; // 设置横向滚动条外层父级的初始宽度
 	var horizontalBarRange = []; // 横向滚动条滚动范围
@@ -172,17 +176,17 @@ function tableSwiperFunc(init) {
 		scroll_body_width = $(scroll_body).width();
 		horizontalBoxAllWidth = wrap_body_width + 5; // 包括内边距
 		horizontalBoxWidth = horizontalBoxAllWidth - 44; // 可滑动的区域宽度
-		if (scroll_body_width <= wrap_body_width) {
+		if (scroll_body_width <= wrap_body_width || $(scroll_body).find('tbody').children().length == 0) {
 			horizontalBarWidth = 0;
-			$(init.horizontal_bar).parent().css({ width: '0', visibility: 'hidden' });
-		} else {
+			$(horizontal_bar).parent().css({ width: '0', visibility: 'hidden' });
+		} else if (scroll_body_width > wrap_body_width && $(scroll_body).find('tbody').children().length != 0) {
 			horizontalBarWidth = (wrap_body_width / scroll_body_width) * horizontalBoxWidth;
-			$(init.horizontal_bar).parent().css({ width: horizontalBoxAllWidth, visibility: 'visible' });
+			$(horizontal_bar).parent().css({ width: horizontalBoxAllWidth, visibility: 'visible' });
 			if (horizontalBarWidth < 50) {
 				horizontalBarWidth = 50;
 			}
 		}
-		$(init.horizontal_bar).width(horizontalBarWidth);
+		$(horizontal_bar).width(horizontalBarWidth);
 		horizontalBarRange = [22, horizontalBoxWidth - horizontalBarWidth + 22]; // 22为左边内边距的距离
 		horizontalAreaRange = [0, scroll_body_width - wrap_body_width];
 	}
@@ -195,7 +199,7 @@ function tableSwiperFunc(init) {
 	function down2(e) {
 		setHorizontalBar();
 		startX = e.clientX;
-		left = $(init.horizontal_bar).position().left;
+		left = $(horizontal_bar).position().left;
 		function move(e) {
 			var newLeft = 0;
 			endX = e.clientX;
@@ -213,7 +217,7 @@ function tableSwiperFunc(init) {
 				$('.scroll-table-fixed-left').addClass('hide');
 			}
 
-			$(init.horizontal_bar).css({
+			$(horizontal_bar).css({
 				left: newLeft
 			});
 			elemTargetX = horizontalAreaRange[1] * ((newLeft - horizontalBarRange[0]) / (horizontalBarRange[1] - horizontalBarRange[0]));
@@ -225,21 +229,21 @@ function tableSwiperFunc(init) {
 			$(document).off('mousemove');
 			$(document).off('mouseup');
 			//释放捕获
-			init.horizontal_bar.releaseCapture && init.horizontal_bar.releaseCapture();
+			horizontal_bar.releaseCapture && horizontal_bar.releaseCapture();
 		}
 		$(document).on('mousemove', move);
 		$(document).on('mouseup', up);
 		//设置捕获
-		init.horizontal_bar.setCapture && init.horizontal_bar.setCapture();
+		horizontal_bar.setCapture && horizontal_bar.setCapture();
 		//阻止浏览器默认事件
 		e.preventDefault && e.preventDefault();
 		return false;
 	}
-	$(init.horizontal_bar).on('mousedown', down2);
+	$(horizontal_bar).on('mousedown', down2);
 	//横向滚动条点击滚动区域自动定位功能
 	$('.scroll-bar-box').on('click', function (e) {
 		var clickPos = e.clientX - $(this).offset().left;
-		var barLeft = parseInt($(init.horizontal_bar).css('left'));
+		var barLeft = parseInt($(horizontal_bar).css('left'));
 		var newLeft = 0;
 		//确定新的滚动条left值
 		if (clickPos <= barLeft) {
@@ -260,7 +264,7 @@ function tableSwiperFunc(init) {
 			} else {
 				$('.scroll-table-fixed-left').addClass('hide');
 			}
-			$(init.horizontal_bar).css({
+			$(horizontal_bar).css({
 				transition: 'left .3s',
 				left: newLeft
 			});
@@ -274,7 +278,7 @@ function tableSwiperFunc(init) {
 			transform(scroll_head, "translateX", -elemTargetX);
 			transform(scroll_body, "translateX", -elemTargetX);
 			setTimeout(function () {
-				$(init.horizontal_bar).css({
+				$(horizontal_bar).css({
 					transition: 'none',
 				});
 				$(scroll_head).css({
@@ -298,9 +302,8 @@ function tableSwiperFunc(init) {
 		});
 		$(".scroll-table-fixed-left").outerWidth(W + 1);
 	}
-	calcLeftTableWidth(init.colNum);
+	calcLeftTableWidth();
 }
-
 
 function calcRowHeight(fixThead) {
 	var row_h = [];
@@ -318,8 +321,90 @@ function calcRowHeight(fixThead) {
 		row_h.push(h);
 	});
 	$(".scroll-table-fixed-left-body tr").each(function (index, item) {
+		if (index==0) {
+			row_h[index] = row_h[index] - 1;
+		}
 		$(item).height(row_h[index]);
 	});
 }
 
+function initTable(init) {
+	if (!init.colNum || init.colNum<0) {
+		init.colNum = 0;
+	}
+	//固定头部
+	var thArray = $(".scroll-table-body").find('thead tr th').clone();
+	var $thead = $('<div class="scroll-table-head"><table class="tb1 tb2"><thead><tr></tr></thead></table></div>');
+	$.each(thArray, function(index,thItem) {
+		$thead.find('tr').append(thItem);
+	});
+	$(".scroll-table-body").before($thead[0]);
+
+	//滚动条
+	var vScrollBarBox = '<div class="v-scroll-bar-box">\
+                                <div class="up-arrow">\
+                                    <i class="iconfont icon-shangjiantoushixinxiao arrow"></i>\
+                                </div>\
+                                <div class="v-scroll-bar"></div>\
+                                <div class="down-arrow">\
+                                    <i class="iconfont icon-xiajiantoushixinxiao arrow"></i>\
+                                </div>\
+                            </div>';
+	$(".scroll-table-body").append(vScrollBarBox);
+	var hScrollBarBox = '<div class="scroll-bar-box">\
+                                <div class="left-arrow">\
+                                    <i class="iconfont icon-zuojiantoushixinxiao arrow"></i>\
+                                </div>\
+                                <div class="scroll-bar"></div>\
+                                <div class="right-arrow">\
+                                    <i class="iconfont icon-youjiantoushixinxiao arrow"></i>\
+                                </div>\
+                            </div>';
+	$(".scroll-table-box").append(hScrollBarBox);
+
+	//左侧固定部分
+	var scrollTableFixedLeftHtml = '<div class="scroll-table-fixed-left hide">\
+										<div class="scroll-table-fixed-left-head">\
+											<table class="tb1 tb2">\
+												<thead>\
+													<tr>\
+													</tr>\
+												</thead>\
+											</table>\
+										</div>\
+										<div class="scroll-table-fixed-left-body">\
+											<table class="tb1 tb2 no_border_top">\
+												<tbody>\
+												</tbody>\
+											</table>\
+										</div>\
+									</div>';
+	$(".scroll-table-box").append(scrollTableFixedLeftHtml);
+
+	var thArrayLimit = $(".scroll-table-body").find('thead tr th').clone().slice(0, init.colNum);
+	$.each(thArrayLimit, function (index, thItem) {
+		$('.scroll-table-fixed-left-head thead tr').append(thItem);
+	});
+
+	$('.scroll-table-body tbody tr').each(function (index1, trItem) {
+		var $newTr = $('<tr></tr>');
+		var tdArray = $(trItem).find('td').clone().slice(0, init.colNum);
+		$.each(tdArray, function(index2,tdItem){
+			$(tdItem).css({ width: $(thArrayLimit[index2]).outerWidth()});
+			$newTr.append(tdItem);
+		});
+		$('.scroll-table-fixed-left-body tbody').append($newTr[0]);
+	});
+	
+	//设置可视区最大高度
+	if (init.maxHeight>=0) {
+		if (init.maxHeight<200) {
+			init.maxHeight = 200;
+		}
+		$('.scroll-table-body').css({maxHeight: init.maxHeight});
+	}
+
+	tableSwiperFunc();
+	calcRowHeight(init.fixThead);
+}
 /*固定表格滑动脚本结束*/
